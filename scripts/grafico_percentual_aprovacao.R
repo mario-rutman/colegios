f53768eb11e8c93048a72bd00adeec311879061d
sillasgonzaga <sillas.gonzaga@gmail.com>
2017-04-08 08:50:09 -0300

    regressao logistica

--- scripts/grafico_percentual_aprovacao.R
+++ scripts/grafico_percentual_aprovacao.R
@@ -1,18 +1,29 @@
 # probabilidade de passar em uma materia dado que o aluno tirou nota x na primeira prova
 
 library(tidyverse)
+library(magrittr)
+library(stringr)
+library(ROCR)
 
 df_2014 <- read_csv2("dados/col_piloto_2014.csv",
                      locale = locale(encoding = "ISO-8859-1"))
 df_2014 <- df_2014[, -1]
+df_2014$ano <- 2014
 
 df_2015 <- read_csv2("dados/col_piloto_2015.csv",
                      locale = locale(encoding = "ISO-8859-1"))
+df_2015$ano <- 2015
 
 df_2016 <- read_csv2("dados/col_piloto_2016.csv",
                      locale = locale(encoding = "ISO-8859-1"))
+df_2016$ano <- 2016
 
-df <- bind_rows(df_2014, df_2015, df_2016)
+
+df_2017 <- read_csv2("dados/col_piloto_2017.csv",
+                     locale = locale(encoding = "ISO-8859-1"))
+df_2017$ano <- 2017
+
+df <- bind_rows(df_2014, df_2015, df_2016, df_2017)
 # ajeitar nome de ensino
 df$ensino %<>% str_to_lower()
 
@@ -49,3 +60,52 @@ df %>%
   ggplot(aes(x = disciplina, y = porc_reprov, fill = grupo_disciplina)) +
     geom_bar(stat = "identity") +
     facet_grid(ensino ~ .)
+
+# regressao logistica p1 ~ passou de ano
+df_p1 <- df %>% filter(prova == "p1")
+df_p1_treino <- df_p1 %>% filter(ano != 2017)
+df_p1_teste <- df_p1 %>% filter(ano == 2017)
+
+model <- glm(passou_de_ano ~ nota, data = df_p1_treino, family=binomial(link='logit'))
+
+fitted.results <- predict(model, newdata = df_p1_teste,
+                          type='response')
+fitted.results.bin <- ifelse(fitted.results > 0.5,1,0)
+
+# confusion matrix
+confmat <- table(df_p1_teste$passou_de_ano, fitted.results.bin)
+confmat
+confmat/length(fitted.results.bin)
+# coeficientes da matriz de confusao
+a = confmat[1,1]
+b = confmat[1,2]
+c = confmat[2,1]
+d = confmat[2,2]
+
+tnr = a/(a+b)
+fpr = b/(a+b)
+
+tpr = d/(c+d)
+fnr = c/(c+d)
+
+# acuracia da matriz de confusao
+(tpr + tnr)/(tnr + fpr + tpr + fnr)
+
+# Acurácia do modelo:
+mean(fitted.results.bin == df_p1_teste$passou_de_ano)
+
+# exibir curva ROC
+pr <- prediction(fitted.results.bin, df_p1_teste$passou_de_ano)
+prf <- performance(pr, measure = "tpr", x.measure = "fpr")
+plot(prf)
+
+auc <- performance(pr, measure = "auc")
+auc <- auc@y.values[[1]]
+auc
+# exibir grafico ggplot
+df_p1_teste$predicted_prob <- fitted.results
+
+ggplot(df_p1_teste, aes(x = nota, y = predicted_prob)) +
+  geom_point() + geom_line()
+
+
